91afe97794f16d2dc6b7a782a2a1312bbaefbb2d
mooncero <97421668+mooncero@users.noreply.github.com>
2024-10-28 12:32:50 +0700

    Create 250-300.js

---
+++ 250-300.js
@@ -0,0 +1,8 @@
+atOptions = {
+    'key' : '0588635e143b313b5e3e078a470bf3ce',
+    'format' : 'iframe',
+    'height' : 250,
+    'width' : 300,
+    'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="//servicefigured.com/0588635e143b313b5e3e078a470bf3ce/invoke.js"></scr' + 'ipt>');
